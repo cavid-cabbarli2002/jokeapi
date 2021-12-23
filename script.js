ffeb853b52831b04eaca8581ab17592690bee872
cavid-cabbarli2002 <cabbarlicavid1@gmail.com>
2021-12-23 15:28:21 +0400

    funksiyalarin sadece adi yaradildi

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('button')
+const audioElement = document.getElementById('audio')
+/*b77bcc5f89c7420fa602f525c845ff25*/
+function zarafatlariGetir () {}
+
+function meniGuldur(joke) {
+
+}
